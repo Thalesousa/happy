1265a82a490d861d0a08e2e807737c04b4b3b59e
thalesousa <thalestjsb@gmail.com>
2020-10-13 18:41:00 -0300

    Aula 2

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import './database/connection';
+import 'express-async-errors';
+
+import cors from 'cors';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'upload')));
+app.use(errorHandler);
+
+
+app.listen(3333);
